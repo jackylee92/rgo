58e0b5f578b00a1685bc90799270d4f088f113b1
李俊东 <lijundong@ruigushop.com>
2023-08-10 14:45:39 +0800

    string to []byte

--- util/rgstring/rgstring.go
+++ util/rgstring/rgstring.go
@@ -1,2 +1,18 @@
 package rgstring
 
+import (
+	"reflect"
+	"unsafe"
+)
+
+func StrToByte(param *string) (data *[]byte) {
+	paramPoint := *(*reflect.StringHeader)(unsafe.Pointer(param))
+	data = (*[]byte)(unsafe.Pointer(&paramPoint))
+	return data
+}
+
+func ByteToStr(param *[]byte) (data *string) {
+	paramPoint := *(*reflect.SliceHeader)(unsafe.Pointer(param))
+	data = (*string)(unsafe.Pointer(&paramPoint))
+	return data
+}
